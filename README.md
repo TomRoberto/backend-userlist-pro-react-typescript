40dade2e57eda90c1f53c8769972248e45f5cd37
Tom ROBERTO <tom.roberto31@gmail.com>
2025-02-07 14:33:33 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# backend-userlist-pro-react-typescript
---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const data = require("./data.json");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/user-list");
+
+const User = mongoose.model("User", {
+  username: String,
+  email: String,
+  age: Number,
+});
+
+app.post("/create-data", async (req, res) => {
+  try {
+    await User.deleteMany();
+    await User.insertMany(data);
+    res.status(201).json({ message: "Data created" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/users", async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.put("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.listen(3000, () => console.log("Server started"));
